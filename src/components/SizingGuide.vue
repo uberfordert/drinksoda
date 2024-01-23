dda69de01f6d06d0ff2ccfbf7935a4d9f77ef710
uberfordert <uberfordert@gmail.com>
2024-01-23 20:01:22 +0100

    sizingguide-goner

--- src/components/SizingGuide.vue
+++ src/components/SizingGuide.vue
@@ -1,28 +1,11 @@
 <template>
     <Transition>
         <div
-            v-if="popUpStore().showSizingGuide"
-            class="mx-6 my-6 bg-fuchsia-500 border border-red-400 text-red-700 px-4 py-3 rounded absolute"
-            role="alert"
         >
             <div class="grid grid-cols-12 grid-rows-3">
                 <button class="col-span-1 col-start-1 row-start-1 text-xs" @click="closeSizingGuide">x</button>
                 <div class="col-start-1 row-start-2 col-span-auto">
                     <table class="table-auto border border-black">
-                        <thead>
-                        <tr>
-                            <th>EU</th>
-                            <th>NA</th>
-                            <th>UK</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th>20</th>
-                            <th>40</th>
-                            <th>30</th>
-                        </tr>
-                        </tbody>
                     </table>
                 </div>
             </div>
