a5b99b0b0fc41b867498628cdd59181be4bc8db0
“israeltshilumbaFHTW” <101051889+israeltshilumba@users.noreply.github.com>
2024-01-25 14:17:54 +0100

    gif works
    gif works

--- src/components/SideBarGif.vue
+++ src/components/SideBarGif.vue
@@ -1,7 +1,12 @@
 <template>
-  <img src="../assets/micki-ficki.gif" alt="Drink Soda Strapped" width="600px" height="auto" />
-
+  <div>
+    <img
+      src="../assets/micki-ficki.gif"
+      alt="Drink Soda Strapped"
+      width="600"
+      height="auto"
+    />
+  </div>
 </template>
 
 <script setup></script>
-
