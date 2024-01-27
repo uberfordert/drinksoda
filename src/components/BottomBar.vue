f521c1d29115d4cc4518bbcc66589c11b22089fd
“israeltshilumbaFHTW” <101051889+israeltshilumba@users.noreply.github.com>
2024-01-27 20:00:15 +0100

    lg font smaller
    footer font smaller

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,15 +1,15 @@
 <template>
   <footer
-    class="lg:col-span-5 lg:col-start-1 pt-10 text-xs lg:text-2xl md:text-lg gap-4"
+    class="lg:col-span-5 lg:col-start-1 pt-10 text-xs lg:text-2xl md:text-lg gap-4 mb-4"
   >
     <div class="grid grid-cols-3 lg:grid-cols-5 lg:content-around">
       <div
-        class="col-start-1 flex items-center justify-center hover:bg-fuchsia-500 hover:text-black rounded-2xl lg:text-4xl text-sm"
+        class="col-start-1 flex items-center justify-center hover:bg-fuchsia-500 hover:text-black rounded-2xl lg:text-3xl text-sm"
       >
         LOOK BOOK
       </div>
       <div
-        class="col-start-3 lg:col-start-5 col-span-1 flex items-center justify-center hover:bg-fuchsia-500 hover:text-black rounded-2xl lg:text-4xl text-sm"
+        class="col-start-3 lg:col-start-5 col-span-1 flex items-center justify-center hover:bg-fuchsia-500 hover:text-black rounded-2xl lg:text-3xl text-sm"
       >
         SIZING GUIDE
       </div>
