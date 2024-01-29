9227a0fd062962d63c6584ccb5d6d4164bc23241
uberfordert <uberfordert@gmail.com>
2024-01-29 19:14:18 +0100

    second produced

--- src/products/products.js
+++ src/products/products.js
@@ -15,6 +15,17 @@ const jsonData = [
         "info": "High quality T-Shirt with front and back-print. All earnings directly support DrinkSoda! 100% Organic Cotton, 200gsm, MADE IN BANGLADESH, HANDPRINTED IN VIENNA",
         "image": require("../assets/drinksoda_tshirt-front.png") // Replace with the actual image URL or filename
     }
+    {
+        "id": 2,
+        "name": "DrinkSoda T-Shirt",
+        "price_euro": 24,
+        "price_ids": {
+            "ONE SIZE": "price_1OQY3uKpJM1RytB8VJK9o0nL"
+        },
+        "sizes": ["ONE SIZE"],
+        "info": "High quality T-Shirt with front and back-print. All earnings directly support DrinkSoda! 100% Organic Cotton, 200gsm, MADE IN BANGLADESH, HANDPRINTED IN VIENNA",
+        "image": require("../assets/drinksoda_tshirt-front.png") // Replace with the actual image URL or filename
+    }
     /*{
         "id": 2,
         "name": "Sweater",
