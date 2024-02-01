ba83b84c6d97bdddff86b2f1603c4f3323b74514
uberfordert <uberfordert@gmail.com>
2024-02-01 23:03:28 +0100

    preis

--- src/components/HoodieView.vue
+++ src/components/HoodieView.vue
@@ -56,7 +56,7 @@
       <div
         class="col-span-5 border rounded-xl lg:m-8 m-4 lg:p-4 p-2 lg:text-xl text-xs"
       >
-        <h1 class="lg:m-8 text-md mb-2">34.20€ - Drink Soda T-Shirt</h1>
+        <h1 class="lg:m-8 text-md mb-1">34.20€ - Drink Soda T-Shirt</h1>
         <p class="">High Quality T-Shirt with back and front print</p>
         <p class="">Printed in Vienna</p>
       </div>
