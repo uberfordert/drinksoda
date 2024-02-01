1d64d416784215760fd172136848cc1651351427
uberfordert <uberfordert@gmail.com>
2024-02-01 23:40:08 +0100

    pr34rf45

--- src/components/SideBarGif.vue
+++ src/components/SideBarGif.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="col-span-5 lg:col-span-1 md:col-span-4">
     <a href="https://www.instagram.com/drinksoda__/" target="_blank" rel="noopener noreferrer">
-  <img src="micki-ficki.gif" alt="@drinksoda__" class="lg:px-5 md:px-5 md:pt-10 lg:pt-10 px-20 w-full lg:col-start-1 col-start-3">
-</a>
-
+    <img
+      class="lg:px-5 md:px-5 md:pt-10 lg:pt-10 px-20 w-full lg:col-start-1 col-start-3"
+      src="../assets/micki-ficki.gif"
+      alt="Drink Soda Strapped"
+    >
+  </a>
   </div>
 </template>
-
-<script setup></script>
