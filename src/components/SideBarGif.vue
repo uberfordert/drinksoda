96c08c932b602bc7f51e830d306e5aeb6888de99
uberfordert <uberfordert@gmail.com>
2024-02-01 23:31:24 +0100

    pr

--- src/components/SideBarGif.vue
+++ src/components/SideBarGif.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-span-5 lg:col-span-1 md:col-span-4">
     <a href="https://www.instagram.com/drinksoda__/" target="_blank" rel="noopener noreferrer">
-  <img src="micki-ficki.gif" alt="@drinksoda__" class="w-full h-auto">
+  <img src="micki-ficki.gif" alt="@drinksoda__" class="lg:px-5 md:px-5 md:pt-10 lg:pt-10 px-20 w-full lg:col-start-1 col-start-3">
 </a>
 
   </div>
