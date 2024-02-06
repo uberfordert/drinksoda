ec1de6a32b51ac593e4d4b159203328886037fd6
uberfordert <uberfordert@gmail.com>
2024-02-06 23:50:37 +0100

    diff-gifss jasaa

--- src/components/SideBarGif.vue
+++ src/components/SideBarGif.vue
@@ -1,30 +1,20 @@
 <template>
   <div class="col-span-5 lg:col-span-1 md:col-span-4">
-    <a href="https://www.instagram.com/drinksoda__/" target="_blank" rel="noopener noreferrer">
+    <!-- Desktop GIF -->
+    <a href="https://www.instagram.com/drinksoda__/" target="_blank" rel="noopener noreferrer" class="hidden lg:block">
       <img
         class="lg:px-5 md:px-5 md:pt-10 lg:pt-10 w-full lg:col-start-1 col-start-3"
-        :src="gifSrc"
+        src="../assets/ds-t-pc.gif"
         alt="Drink Soda Strapped"
+      >
+    </a>
+    <!-- Mobile GIF -->
+    <a href="https://www.instagram.com/drinksoda__/" target="_blank" rel="noopener noreferrer" class="block lg:hidden">
+      <img
+        class="lg:px-5 md:px-5 md:pt-10 lg:pt-10 w-full lg:col-start-1 col-start-3"
+        src="../assets/ds-t.gif"
+        alt="Drink Soda Strapped (Mobile)"
       >
     </a>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      desktopGif: "../assets/ds-t-pc.gif",
-      mobileGif: "../assets/ds-t.gif"
-    };
-  },
-  computed: {
-    gifSrc() {
-      return this.isMobile ? this.mobileGif : this.desktopGif;
-    },
-    isMobile() {
-      return window.innerWidth < 768; // Adjust breakpoint as needed
-    }
-  }
-};
-</script>
