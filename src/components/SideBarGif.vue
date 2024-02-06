f784266c688df936d2e16af44fa5e04fa01bc22a
uberfordert <uberfordert@gmail.com>
2024-02-06 23:42:59 +0100

    diff-gif

--- src/components/SideBarGif.vue
+++ src/components/SideBarGif.vue
@@ -3,9 +3,24 @@
     <a href="https://www.instagram.com/drinksoda__/" target="_blank" rel="noopener noreferrer">
       <img
         class="lg:px-5 md:px-5 md:pt-10 lg:pt-10 w-full lg:col-start-1 col-start-3"
-        src="../assets/drinksoda-shirt.gif"
+        id="sodaGif"
         alt="Drink Soda Strapped"
       >
     </a>
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    const imgElement = document.getElementById('sodaGif');
+
+    if (isMobile) {
+      imgElement.src = "../assets/ds-t.gif";
+    } else {
+      imgElement.src = "../assets/ds-t-pc.gif";
+    }
+  }
+}
+</script>
