66c9483ac2f63b08c8e9fa6bd2e092adacbe344d
uberfordert <uberfordert@gmail.com>
2024-04-09 20:17:38 +0200

    no header

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,9 +1,4 @@
 <template>
-  <img
-    alt="no image"
-    class="mx-auto rounded-2xl lg:h-32 md:h-16 h-16 m-4"
-    src="../assets/drinksoda_header.png"
-  />
 </template>
 
 <script setup></script>
